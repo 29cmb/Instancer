61f673e5954957fc6d0b76e6eb50dbbff4dcf198
Cmb <71194682+29cmb@users.noreply.github.com>
2024-06-30 16:24:47 -0400

    instancer setup

--- main.lua
+++ main.lua
@@ -2,6 +2,10 @@ local Instance = require("instancer.instance")
 function love.load()
     local ins = Instance.new()
     print(ins:getName())
+    local ins2 = Instance.new()
+    ins2.Name = "thing2"
+    print(ins:getName())
+    print(ins2:getName())
 end
 
 function love.draw()
